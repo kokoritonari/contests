d2cc878d9c400eb3adf7ac2bdb8081def57bc398
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-02-25 05:38:17 +0900

    :sparkles: AtCoder Programming Guide for begginers(APG4b)にcppで挑戦してみた

---
+++ AtCoder/APG4b/111.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+
+  // ここにプログラムを追記
+  string op;
+  int B;
+  for (int i = 0; i < N; i++) {
+    cin >> op >> B;
+    if (op == "+") A += B;
+    else if (op == "-") A -= B;
+    else if (op == "*") A *= B;
+    else if (op == "/" && B != 0) A /= B;
+    else {cout << "error" << endl; break;}
+    cout << i + 1 << ":" << A << endl;
+  }
+}

e3f6e696ec31657e2548b7e5c1c0e109ecf31f04
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-04-23 22:47:18 +0900

    Change library.sh

--- library.sh
+++ library.sh
@@ -2,3 +2,6 @@
 for f in $(ls); do
   git mv $f $(echo $f | sed -E 's/^(ITP1_[0-9][0-9]?_[A-D]).+\.(.+)/\1.\2/g')
 done
+
+# create file with the same name but a different extension
+ls ITP1_1_* | sed s/.py/.cs/ | xargs touch
